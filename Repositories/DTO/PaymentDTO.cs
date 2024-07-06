72daecbfd18bfd8949ae2a3646de8ceed533c052
DaoBaoKha <daobaokha2509@gmail.com>
2024-07-06 23:36:57 +0700

    updatePayment

--- Repositories/DTO/PaymentDTO.cs
+++ Repositories/DTO/PaymentDTO.cs
@@ -4,8 +4,10 @@
     {
         public int PaymentId { get; set; }
         public int? BookingId { get; set; }
+        public int? UserId { get; set; } // Thêm dòng này để lưu thông tin UserId cho nạp tiền
         public DateTime? PaymentDate { get; set; }
         public double? PaymentAmount { get; set; }
         public double? TotalAmount { get; set; }
+        public string PaymentCode { get; set; } // Thêm dòng này nếu cần lưu mã thanh toán
     }
 }
--- Services/IVNPayService.cs
+++ Services/IVNPayService.cs
@@ -8,10 +8,10 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-
     public interface IVNPayService
     {
         ResponseUriModel CreatePayment(PaymentInfoModel model, HttpContext context);
+        ResponseUriModel CreateDeposit(PaymentInfoModel model, HttpContext context, int userId); 
         PaymentResponseModel PaymentExecute(IQueryCollection collection);
     }
 }
